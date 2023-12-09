42dc35550e4e3dcbe691386ccf8edcf43dba6f64
AnupamDutta3 <134681181+AnupamDutta3@users.noreply.github.com>
2023-12-09 09:41:44 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+  default = "abc"
+}
+variable "instance_type" {
+  default = "from-module"
+}
+variable "key_name" {
+  default = "something"
+}
+variable "instance_name" {
+  default = "something"
+}
+variable "number" {
+  default = 1
+}
