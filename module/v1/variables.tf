75cd01d75ce0f11c5c820e46d4873f83210fc02b
AnupamDutta3 <134681181+AnupamDutta3@users.noreply.github.com>
2023-12-09 07:56:06 +0530

    Update variables.tf

--- module/v1/variables.tf
+++ module/v1/variables.tf
@@ -1,1 +1,15 @@
-
+vairable "ami" {
+  default = "ami-0fc5d935ebf8bc3bc"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "key_name" {
+  default = "something"
+}
+variable "instance_name" {
+  default = "something"
+}
+variable "count" {
+  default = 1
+}
