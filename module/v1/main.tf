bf8789ed850767235e84b7afd60b93e89329ad07
AnupamDutta3 <134681181+AnupamDutta3@users.noreply.github.com>
2023-12-09 08:08:06 +0530

    Update main.tf

--- module/v1/main.tf
+++ module/v1/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "TF_instance_1" {
   ami           = "${var.ami}"
   instance_type = "{$var.instance_type}"
-  key_name      = "${var.key}"
-  count = ${var.count}
+  key_name      = "${var.key_name}"
+  count = var.number
 
   tags = {
     Name = "${var.instance_name}"
