9810d758e2c015d5c32fb4d9b43257f0e6d25c88
AnupamDutta3 <134681181+AnupamDutta3@users.noreply.github.com>
2023-12-09 07:43:50 +0530

    Create main.tf

---
+++ module/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "TF_instance_1" {
+  ami           = "${var.ami}"
+  instance_type = "{$var.instance_type}"
+  key_name      = "${var.key}"
+  count = 3
+
+  tags = {
+    Name = "var.instance_name"
+  }
