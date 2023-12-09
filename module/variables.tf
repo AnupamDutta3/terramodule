362350957a8e097bd340d9703dbf92db0b497479
AnupamDutta3 <134681181+AnupamDutta3@users.noreply.github.com>
2023-12-09 07:46:53 +0530

    Create variables.tf

---
+++ module/variables.tf
@@ -0,0 +1,18 @@
+vairable "ami" {
+  default = "ami-0fc5d935ebf8bc3bc"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "key_name" {
+  default = "something"
+}
+variable "instance_name" {
+  default = "something"
+}
+variable "count" {
+  default = 1
+}
+
+
+  
